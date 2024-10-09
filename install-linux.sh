867f0ef6890c0921c2b53c03ec89ab0ff365c201
Krystian Bajno <krystian.bajno@gmail.com>
2024-10-09 06:00:48 +0200

    linux

--- install-linux.sh
+++ install-linux.sh
@@ -2,15 +2,15 @@
 
 sudo apt update
 sudo apt install -y python3 python3-pip python3-venv
-sudo apt install -y nodejs npm
+sudo apt install python3-playwright
 
 python3 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
 pip install pytest-playwright playwright -U
-playwright install-deps
 
-playwright install
+playwright install-deps
+playwright install --with-deps
 
 echo "Installation complete!"
